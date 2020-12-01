520b69d3875c6f91ab5e62689e39829c2c7dcc82
mEm0ry <380080610@qq.com>
2020-12-01 21:06:44 +0800

    计算n的阶乘

---
+++ 分支与循环练习/计算n的阶乘/test.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int main()             //计算n的阶乘
+{	
+	int i = 0;
+	int num = 1;
+	int n = 0;
+	printf("请输入n的值：\n");
+	scanf("%d",&n);
+	for(i=1 ; i<=n ; i++)
+	{
+		num = num*i;
+	}
+	printf("%d\n",num);
+
+	return 0;
+}
