4bc266f1cadbe4b5ce132cef285da6c78916414b
mEm0ry <380080610@qq.com>
2020-12-02 21:58:37 +0800

    计算1!+2!+.....+10!

---
+++ 分支与循环练习/计算1!+2!+.....+10!/test.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	int i = 0;                 //计算1!+2!+.....+10!
+	int num = 1;
+	int n = 0;
+	int result = 0;
+	for(n=1; n<=10;n++)    //控制n的值1-10
+	{
+		for(i=1;i<=n;i++)  //计算n的阶乘
+		{
+			num = num*i;
+		}
+		result=result+num; 
+	}
+	printf("%d",result);
+
+	return 0;
+}
