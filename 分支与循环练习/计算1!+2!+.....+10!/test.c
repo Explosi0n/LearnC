73b3afe39d82ab0a7f2ac16f15154a53a4a3e336
mEm0ry <380080610@qq.com>
2020-12-03 19:57:23 +0800

    计算1!+2!+.....+10!

--- 分支与循环练习/计算1!+2!+.....+10!/test.c
+++ 分支与循环练习/计算1!+2!+.....+10!/test.c
@@ -6,14 +6,16 @@ int main()
 	int num = 1;
 	int n = 0;
 	int result = 0;
-	for(n=1; n<=10;n++)    //控制n的值1-10
-	{
+	for(n=1; n<=3;n++)    //控制n的值1-10
+	{   
+		
 		for(i=1;i<=n;i++)  //计算n的阶乘
 		{
-			num = 1;
+			
 			num = num*i;
 		}
 		result=result+num; 
+		num = 1;
 	}
 	printf("%d",result);
 
