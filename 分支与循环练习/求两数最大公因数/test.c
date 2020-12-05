f6c48f475d459afa727253621787e7bbd54f400b
mEm0ry <380080610@qq.com>
2020-12-05 19:55:28 +0800

    求两数最大公因数

---
+++ 分支与循环练习/求两数最大公因数/test.c
@@ -0,0 +1,27 @@
+﻿#include<stdio.h>
+
+int main()                         //求两数最大公因数
+{
+	int n = 0;
+	int m = 0;
+	int r = 1;
+	printf("请输入两个数：");
+	scanf("%d%d",&n,&m);
+
+	while(r)
+	{
+		r = n%m;
+
+		if(r!= 0)
+		{
+			n = m;
+			m = r;
+		}
+		else
+		{
+			printf("最大公约数是%d\n",m);
+		}
+	}
+
+	return 0;
+}
