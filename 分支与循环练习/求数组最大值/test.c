f478481b3cf143680e4a5fbe390fa213a5a41bed
mEm0ry <380080610@qq.com>
2020-12-07 20:20:24 +0800

    求数组最大值

---
+++ 分支与循环练习/求数组最大值/test.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+
+int main()                                       //求数组最大值
+{
+	int arr[]={2,2,3,4,5,6,7,7,8,9,12,3,5};
+	int max = arr[0];                            //int max = 0存在局限性    
+	int sz = sizeof(arr)/sizeof(arr[0]);
+	int i = 0 ;
+	
+	for(i=0;i<sz;i++)
+	{
+		if(arr[i]>max)
+		{
+			max = arr[i];
+		}
+	}
+	printf("最大值为%d\n",max);
+
+	return 0;
+}
