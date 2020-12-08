6c90c6dfe1fb6b4c39d2b9cc3b0ee431334dc844
mEm0ry <380080610@qq.com>
2020-12-08 17:03:20 +0800

    打印九九乘法表

---
+++ 分支与循环练习/九九乘法表/test.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+int main()                     //打印99乘法表
+{
+	int i = 0;
+	int j = 0;
+	for(i=1; i<=9; i++)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf("%dx%d=%-2d ",i,j,i*j);   
+		}
+		putchar('\n');
+	}
+	return 0;
+}
